d87a4a863c44e2ab7845e0a75c8702eded262cfe
TwAzuremy <958559114@qq.com>
2024-12-20 00:03:30 +0800

    new URL class is changed to a URI class

--- src/minecraft/uuid/UUIDGenerator.java
+++ src/minecraft/uuid/UUIDGenerator.java
@@ -2,6 +2,8 @@ package minecraft.uuid;
 
 import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.UUID;
 
@@ -10,8 +12,8 @@ public class UUIDGenerator {
 
     static {
         try {
-            url = new URL("https://www.uuidtools.com/api/generate/v4");
-        } catch (MalformedURLException e) {
+            url = new URI("https://www.uuidtools.com/api/generate/v4").toURL();
+        } catch (MalformedURLException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
@@ -47,16 +49,16 @@ public class UUIDGenerator {
         UUID uuid = UUID.fromString(uuidFromString);
 
         // 获取 UUID 的高64位和低64位
-        long high64 = uuid.getMostSignificantBits();  // 高64位
-        long low64 = uuid.getLeastSignificantBits();   // 低64位
+        long high64 = uuid.getMostSignificantBits(); // 高64位
+        long low64 = uuid.getLeastSignificantBits(); // 低64位
 
         // 将高64位分成两个 32 位整数
-        int high64High32 = (int) (high64 >> 32);  // 高64位的高32位
-        int high64Low32 = (int) (high64 & 0xFFFFFFFFL);  // 高64位的低32位
+        int high64High32 = (int) (high64 >> 32); // 高64位的高32位
+        int high64Low32 = (int) (high64 & 0xFFFFFFFFL); // 高64位的低32位
 
         // 将低64位分成两个 32 位整数
-        int low64High32 = (int) (low64 >> 32);  // 低64位的高32位
-        int low64Low32 = (int) (low64 & 0xFFFFFFFFL);  // 低64位的低32位
+        int low64High32 = (int) (low64 >> 32); // 低64位的高32位
+        int low64Low32 = (int) (low64 & 0xFFFFFFFFL); // 低64位的低32位
 
         return String.format("[I;%d,%d,%d,%d]", high64High32, high64Low32, low64High32, low64Low32);
     }
